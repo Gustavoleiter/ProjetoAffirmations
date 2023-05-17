2b01da5337427563504ec399f74ec2d8904c97b7
Gustavo <gustavu0720@gmail.com>
2023-05-17 20:49:22 -0300

    imagens adicionadas

--- app/src/main/java/com/ds2/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/ds2/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ds2.affirmations.R
@@ -16,6 +17,7 @@ class ItemAdapter(private val context: Context,
     class ItemViewHolder(private val view: View) :
         RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -27,6 +29,7 @@ class ItemAdapter(private val context: Context,
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResoureId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/ds2/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/ds2/affirmations/model/Affirmation.kt
@@ -1,7 +1,8 @@
 package com.ds2.affirmations.model
 
 data class Affirmation (
-    val stringResoureId: Int
+    val stringResoureId: Int,
+    val imageResourceId: Int
 
     ){
      }
